9f52ee572e2b21280d09601af880cfd129e3c7ff
Alexandre Decan <alexandre.decan@lexpage.net>
2023-05-09 11:31:39 +0200

    Update dependencies

--- genja.py
+++ genja.py
@@ -5,7 +5,7 @@ import shutil
 import ruamel.yaml as yaml
 
 from dateutil.parser import parse as parse_date
-from jinja2 import FileSystemLoader, Environment, contextfilter
+from jinja2 import FileSystemLoader, Environment, pass_context
 
 
 TEMPLATES_PATH = 'templates'
@@ -14,7 +14,7 @@ CONTENT_PATH = 'content'
 OUTPUT_PATH = 'output'
 
 
-@contextfilter
+@pass_context
 def relative_url(context, targetpath):
     basepath = context['page']
     basedir = os.path.dirname(basepath)
@@ -34,7 +34,7 @@ class Genja(object):
         self._static = STATIC_PATH if static is None else static
         self._content = CONTENT_PATH if content is None else content
         self._output = OUTPUT_PATH if output is None else output
-        
+
     def clean(self):
         # Remove output directory content if it exists
         if os.path.exists(self._output):
@@ -45,17 +45,17 @@ class Genja(object):
                     shutil.rmtree(os.path.join(dirpath, dirname))
         else:
             os.makedirs(self._output, exist_ok=True)
-    
+
     def build(self):
         self._build_content()
         self._build_static()
-        
+
     def watch(self):
         pass
-    
+
     def serve(self):
         pass
-    
+
     def _build_static(self):
         # Copy static content
         for item in os.listdir(self._static):
@@ -65,13 +65,13 @@ class Genja(object):
                 shutil.copytree(source, destination)
             else:
                 shutil.copy2(source, destination)
-        
+
     def _build_content(self):
         # Create environment for Jinja2
         env = Environment(
             loader=FileSystemLoader([self._templates, self._content]),
         )
-        
+
         # Configure environment
         env.globals.update({
             'load_yaml': lambda fp: load_yaml(os.path.join(self._content, fp))
@@ -79,18 +79,18 @@ class Genja(object):
         env.filters.update({
             'relative': relative_url,
         })
-        
+
         # Render all *.html pages if the name does not start with an underscore
         for (dirpath, dirnames, filenames) in os.walk(self._content):
             for filename in filenames:
                 filepath = os.path.join(dirpath, filename)[len(self._content) + 1:]
-                
+
                 if filepath.startswith('_') or not filepath.endswith('.html'):
                     print('Ignoring {}'.format(filepath))
                     continue
                 print('Rendering {}'.format(filepath))
                 template = env.get_template(filepath)
-                
+
                 # Create output file
                 output_filepath = os.path.join(self._output, filepath)
                 os.makedirs(os.path.dirname(output_filepath), exist_ok=True)
